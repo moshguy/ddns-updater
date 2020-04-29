0e58e8e1ae2d520b01fa0a489ac82ff74650ee0a
moshguy <business_email@moshguy.tech>
2020-04-28 21:53:49 -0400

    Update digitalocean-ddns-updater.sh

--- digitalocean-ddns-updater.sh
+++ digitalocean-ddns-updater.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#The Digitalocean ddns updater is meant to be run as a script from the command line.  There are two parameters -d for the domain to be updated and -t for your Digitalocean api token
+
 printf "$(date)\n"
 
 while getopts t:d: option
@@ -14,13 +16,14 @@ done
 
 #TOKEN="Get token from https://cloud.digitalocean.com/settings/applications"
 #DOMAIN=example.com
-#RECORD_ID=12345
+
+#Get external IP address
 IP=`curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+'`
 
 
-# to get record id:
+# Get all records for domain and parse out RECORD_ID
 RECORD_ID=`curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records" | jq '.domain_records[] | select(.type == "A") | .id'`
 echo $RECORD_ID
 
-# update domain record
+# Update domain record
 curl -s -X PUT -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d "{\"data\":\"$IP\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
